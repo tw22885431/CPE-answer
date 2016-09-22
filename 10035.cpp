3b0f7a30cac052cde9dc68c20b935431d234fc5e
Ben Wu <re22885431@gmail.com>
2016-09-22 17:18:38 +0800

    CPE answer

---
+++ 10035.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std ;
+
+int main(void)
+{
+int iNumber1[i];
+int iNumber2[j];
+int iCount;
+for (int x; x<=iCount; x++)
+    {
+    cin >> i >> j ;
+    } 
+	
+}
